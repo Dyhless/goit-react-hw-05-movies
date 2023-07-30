eaebe282b2bdea1836d289be080d5ed2d21a5574
Dmytro Medvediev <dsisitkin@gmail.com>
2023-07-30 18:39:21 +0200

    add herer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
-export const App = () => {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MoviesPage from './pages/MoviesPage';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+      </Routes>
+    </Router>
   );
 };
+
+export default App;
