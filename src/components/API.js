6296527613962309dff55fd1cd7261dbd278185e
Dmytro Medvediev <dsisitkin@gmail.com>
2023-07-31 21:42:36 +0200

    Add API

---
+++ src/components/API.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+
+const API_KEY = 'b4ef19b8e9db5b53dec85dc073650305'; 
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const trending_movies = '/trending/all/day';
+const search_movies = '/search/movie';
+const movie_details = '/movie';
+const movie_creadits = '/credits';
+const movie_reviews = '/reviews';
+
+const getMovie = axios.create({
+  baseURL: API_BASE_URL,
+  params: {
+    api_key: API_KEY,
+  },
+});
+
+export const getTrendingMovies = async () => {
+  try {
+    const response = await getMovie.get(trending_movies);
+    return response.data.results;
+  } catch (error) {
+    throw new Error('Failed to fetch trending movies');
+  }
+};
+
+export const searchMovies = async (query) => {
+  try {
+    const response = await getMovie.get(search_movies, {
+      params: { query },
+    });
+    return response.data.results;
+  } catch (error) {
+    throw new Error('Failed to search movies');
+  }
+};
+
+export const getMovieDetails = async (movieId) => {
+  try {
+    const response = await getMovie.get(`${movie_details}/${movieId}`);
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch movie details');
+  }
+};
+
+export const getMovieCredits = async (movieId) => {
+  try {
+    const response = await getMovie.get(`${movie_details}/${movieId}${movie_creadits}`);
+    return response.data.cast;
+  } catch (error) {
+    throw new Error('Failed to fetch movie credits');
+  }
+};
+
+export const getMovieReviews = async (movieId) => {
+  try {
+    const response = await getMovie.get(`${movie_details}/${movieId}${movie_reviews}`);
+    return response.data.results;
+  } catch (error) {
+    throw new Error('Failed to fetch movie reviews');
+  }
+};
