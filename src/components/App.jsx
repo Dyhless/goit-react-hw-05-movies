1b26b7ed0c5505e72427d3a6070e4761ef7b5ff4
Dmytro Medvediev <dsisitkin@gmail.com>
2023-08-07 21:59:51 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import { Route, Routes } from 'react-router-dom';
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
 import Layout from './Layout/Layout';
+import Loader from './Loader/Loader';
 
 const Home = lazy(() => import('./pages/Home/Home'));
 const Movies = lazy(() => import('./pages/Movies/Movies'));
@@ -9,15 +10,22 @@ const MovieDetails = lazy(() => import('./pages/MovieDetails/MovieDetails'));
 const Cast = lazy(() => import('./Cast/Cast'));
 const Reviews = lazy(() => import('./Reviews/Reviews'));
 
+const lazyWithLoader = Component => props =>
+  (
+    <Suspense fallback={<Loader />}>
+      <Component {...props} />
+    </Suspense>
+  );
+
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="movies" element={<Movies />} />
-        <Route path="movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
+        <Route index element={lazyWithLoader(Home)()} />
+        <Route path="movies" element={lazyWithLoader(Movies)()} />
+        <Route path="movies/:movieId" element={lazyWithLoader(MovieDetails)()}>
+          <Route path="cast" element={lazyWithLoader(Cast)()} />
+          <Route path="reviews" element={lazyWithLoader(Reviews)()} />
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Route>
