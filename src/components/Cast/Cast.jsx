2d033e580763ab16de85ae5f807b306bca9627da
Dmytro Medvediev <dsisitkin@gmail.com>
2023-08-10 22:56:25 +0200

    update cast rev

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,11 +1,10 @@
-import PropTypes from 'prop-types';
-import { ToastContainer, toast, Slide } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Loader from '../Loader/Loader';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Loader from '../Loader/Loader';
 import { getMovieCredits } from '../API';
 import { Img, List, Text } from './Cast.styled';
+import { ToastContainer, toast, Slide } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Cast = () => {
   const { movieId } = useParams();
@@ -16,8 +15,8 @@ const Cast = () => {
     const onActorsOfMovie = async () => {
       setLoading(true);
       try {
-        const actors = await getMovieCredits(movieId);
-        setActors(actors);
+        const actorsData = await getMovieCredits(movieId);
+        setActors(actorsData);
       } catch {
         toast.error('Sorry. No info.', {
           position: toast.POSITION.TOP_RIGHT,
@@ -30,39 +29,36 @@ const Cast = () => {
     onActorsOfMovie();
   }, [movieId]);
 
+  const noImage =
+    'https://www.bilo-teploservice.info-gkh.com.ua/assets/images/noimage.png';
+
   return (
     <div>
       <ToastContainer transition={Slide} />
       {loading && <Loader />}
-      <List>
-        {actors.map(({ id, profile_path, name, character }) => (
-          <li key={id}>
-            <Img
-              width="200px"
-              src={
-                profile_path
-                  ? `https://image.tmdb.org/t/p/w500${profile_path}`
-                  : `https://www.bilo-teploservice.info-gkh.com.ua/assets/images/noimage.png`
-              }
-              alt={name}
-            />
-            <Text>{name}</Text>
-            <Text>Character: {character}</Text>
-          </li>
-        ))}
-      </List>
+      {actors.length > 0 ? (
+        <List>
+          {actors.map(({ id, profile_path, name, character }) => (
+            <li key={id}>
+              <Img
+                width="200px"
+                src={
+                  profile_path
+                    ? `https://image.tmdb.org/t/p/w500${profile_path}`
+                    : noImage
+                }
+                alt={name}
+              />
+              <Text>{name}</Text>
+              <Text>Character: {character}</Text>
+            </li>
+          ))}
+        </List>
+      ) : (
+        <p>No actors information available.</p>
+      )}
     </div>
   );
 };
 
-Cast.propTypes = {
-  actors: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      id: PropTypes.number.isRequired,
-      profile_path: PropTypes.string,
-      character: PropTypes.number.isRequired,
-    })
-  ),
-};
 export default Cast;
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,10 +1,9 @@
-import PropTypes from 'prop-types';
-import { ToastContainer, toast, Slide } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Loader from 'components/Loader/Loader';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMovieReviews } from '../API';
+import { ToastContainer, toast, Slide } from 'react-toastify';
+import Loader from 'components/Loader/Loader';
+import 'react-toastify/dist/ReactToastify.css';
 import { Item, List } from './Reviews.styled';
 
 const Reviews = () => {
@@ -12,17 +11,19 @@ const Reviews = () => {
   const [reviews, setReviews] = useState([]);
   const [loading, setLoading] = useState(false);
 
+  const errorToastText = 'Ooops... Something went wrong';
+
   useEffect(() => {
     const fetchReviewsFilms = async () => {
       setLoading(true);
       try {
-        const reviews = await getMovieReviews(movieId);
-        setReviews(reviews);
+        const reviewsData = await getMovieReviews(movieId);
+        setReviews(reviewsData);
       } catch {
-        toast.error('Ooops...Something went wrong', {
+        toast.error(errorToastText, {
           position: toast.POSITION.TOP_RIGHT,
           autoClose: 2000,
-        }); //повідомлення у разі помилки
+        });
       } finally {
         setLoading(false);
       }
@@ -30,34 +31,21 @@ const Reviews = () => {
     fetchReviewsFilms();
   }, [movieId]);
 
+  const noReviewsContent = <div>No reviews</div>;
+  const reviewsContent = reviews.map(({ author, content, id }) => (
+    <Item key={id}>
+      <h2>Author: {author}</h2>
+      <p>{content}</p>
+    </Item>
+  ));
+
   return (
     <>
       <ToastContainer transition={Slide} />
       {loading && <Loader />}
-
-      <List>
-        {reviews.length === 0 ? (
-          <div>We don't have any reviews for this movie</div>
-        ) : (
-          reviews.map(({ author, content, id }) => (
-            <Item key={id}>
-              <h2>Author: {author}</h2>
-              <p>{content}</p>
-            </Item>
-          ))
-        )}
-      </List>
+      <List>{reviews.length === 0 ? noReviewsContent : reviewsContent}</List>
     </>
   );
 };
 
-Reviews.propTypes = {
-  reviews: PropTypes.arrayOf(
-    PropTypes.shape({
-      author: PropTypes.string.isRequired,
-      id: PropTypes.number.isRequired,
-      content: PropTypes.string.isRequired,
-    })
-  ),
-};
 export default Reviews;
