41c5ad220034e0d615bb55048fbfaea88c3c5844
Dmytro Medvediev <dsisitkin@gmail.com>
2024-11-01 18:26:57 +0100

    Update index

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-hw-05-movies">
+    <BrowserRouter basename="movies-finder">
       <App />
     </BrowserRouter>
   </React.StrictMode>
